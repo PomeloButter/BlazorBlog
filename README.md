e3ca2a436d3ef11842fdc5446810bd475d94343c
Pomelobutter <Pomelobutter@hotmail.com>
2019-09-30 10:27:31 +0800

    Query article list based on tag

--- README.md
+++ README.md
@@ -3,5 +3,3 @@ A single user blog system based on ASP.NET Core Blazor
 - Asp.Net Core 3.0
 - Blazor
 - Microsoft.EntityFrameworkCore.Sqlite 3.0.0
-
-
--- src/Blog/Api/PostApi.cs
+++ src/Blog/Api/PostApi.cs
@@ -4,50 +4,67 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Blog.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Api
 {
     public partial class Api
     {
-      
         public async Task<Post> GetPostByIdAsync(string id)
         {
-            return  await _context.Posts.SingleOrDefaultAsync(s => s.Id == id);
+            return await _context.Posts.SingleOrDefaultAsync(s => s.Id == id);
         }
 
-        public async Task<PagedViewModel<PostViewModel>> GetPostListAsync(int pageIndex=1,int pageSize=4)
+        public async Task<PagedViewModel<PostViewModel>> GetPostListAsync(string tag, int pageIndex = 1,
+            int pageSize = 2)
         {
-          var list=   _context.Posts.OrderByDescending(x=>x.CreationTime).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
-          var count =await _context.Posts.CountAsync();
-          var pageCount= (count + pageSize - 1)/pageSize;
-          var postModels = list.Select(x => new PostModel
-          {
-              Id = x.Id,
-              Title = x.Title,
-              Url = x.Url,
-              CreationTime = Convert.ToDateTime(x.CreationTime).ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-us")),
-              Year = Convert.ToDateTime(x.CreationTime).Year
-          });
-          List<PostViewModel> result = new List<PostViewModel>();
-          var group = postModels.GroupBy(x => x.Year).ToList();
-          group.ForEach(x =>
-          {
-              result.Add(new PostViewModel
-              {
-                  Year = x.Key,
-                  Posts = x.ToList()
-              });
-          });
-            return new PagedViewModel<PostViewModel>()
-          {
-              Data = result,
-              CurrentPage = pageIndex,
-              TotalCount = count,
-              TotalPage = pageCount
-          };
+            var list = new List<Post>();
+            var count = 0;
+            if (!string.IsNullOrEmpty(tag))
+            {
+                var tagEntity = await _context.Tags.SingleOrDefaultAsync(s => s.DisplayName == tag);
+                var postTags = _context.PostTags.ToList().FindAll(s => s.TagId == tagEntity.Id);
+                var list1 = list;
+                postTags.ForEach(x => { list1.Add(_context.Posts.SingleOrDefault(s => s.Id == x.Id)); });
+                list = list1.OrderByDescending(x => x.CreationTime).Skip((pageIndex - 1) * pageSize).Take(pageSize)
+                    .ToList();
+                count = postTags.Count;
+            }
+            else
+            {
+                list = _context.Posts.OrderByDescending(x => x.CreationTime).Skip((pageIndex - 1) * pageSize)
+                    .Take(pageSize).ToList();
+                count = _context.Posts.Count();
+            }
+
+           
+            var pageCount = (count + pageSize - 1) / pageSize;
+            var postModels = list.Select(x => new PostModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Url = x.Url,
+                CreationTime = Convert.ToDateTime(x.CreationTime)
+                    .ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-us")),
+                Year = Convert.ToDateTime(x.CreationTime).Year
+            });
+            var result = new List<PostViewModel>();
+            var group = postModels.GroupBy(x => x.Year).ToList();
+            group.ForEach(x =>
+            {
+                result.Add(new PostViewModel
+                {
+                    Year = x.Key,
+                    Posts = x.ToList()
+                });
+            });
+            return new PagedViewModel<PostViewModel>
+            {
+                Data = result,
+                CurrentPage = pageIndex,
+                TotalCount = count,
+                TotalPage = pageCount
+            };
         }
     }
 }
