71ca83af18920cdbe805085b60124b83e5b08b15
Colin Ji <colin.ji@xian.superprogrommers.net>
2019-09-30 08:35:52 +0800

    fix bug

--- src/Blog/wwwroot/js/common.js
+++ src/Blog/wwwroot/js/common.js
@@ -1,11 +1,13 @@
-﻿function mobileBtn() {
+﻿
+function mobileBtn() {
     var toggleMenu = document.getElementsByClassName("menu-toggle")[0];
     var mobileMenu = document.getElementById("mobile-menu");
     if (toggleMenu.classList.contains("active")) {
         toggleMenu.classList.remove("active");
         mobileMenu.classList.remove("active");
-    } else {
+    } else {;
         toggleMenu.classList.add("active");
         mobileMenu.classList.add("active");
     }
 }
+
